007b73f46ab10f5573967112cef0bf9646e08186
Vincent Shih-Ming Huang <vincent.sm.huang@gmail.com>
2012-06-01 16:00:56 +0800

    Add encryption and decryption scripts

---
+++ dec.sh
@@ -0,0 +1,73 @@
+function decrypt_one()
+{
+	echo -ne "\033[1;36m$1\033[0m: "
+	len=${#1}
+	file_type1=${1:len-5}
+	gz_name=${1:0:len-5}
+	file_name=${1:0:len-8}
+	len_fn=${#file_name}
+	file_type_tar=${file_name:len_fn-4}
+
+	# Decrypt a file.  Check if it is .des format first
+	if [ $file_type1 != ".des3" ]; then
+		echo -e "The given file is not encrypted by DES3"
+		exit 0
+	fi
+
+	# Start decrypting
+	echo -ne "\033[1;34mDecrypting\033[0m ..."
+	openssl enc -des3 -d -in "$1" -out "$gz_name" -kfile ~/Desktop/.key
+
+	echo -ne "\033[1;35mDecompressing\033[0m ... "
+	if [ $file_type_tar == ".tar" ]
+	then
+		tar zxvf "$gz_name"
+		rm -f "$gz_name"
+	else
+		gzip -d "$gz_name"
+	fi
+
+	echo -e "\033[1;32mdone\033[0m"
+}
+
+function decrypt_all()
+{
+
+	for i in *.des3
+	do
+		decrypt_one "$i"
+	done
+
+	rm -f *.des3
+}
+
+# Check if argument is given.
+if [ -z "$1" ]
+then
+	# If there is no given argument, recursively decrypt file one-by-one.
+	echo -e
+	echo -e "\033[1;32m === Decrypting all files under current directory \033[0m"
+	echo -e
+	decrypt_all
+	echo -e
+	echo -e
+	echo -e "\033[1;32m === decryption finished === \033[0m"
+else
+	# Check if the given argument is a file or a directory
+	if [ -d "$1" ]
+	then
+		# Cannot decrypt a directory
+		echo -e "Sorry. Cannot decrypt a directory."
+		echo -e "Please give a \033[1;33m.des\033[0m file."
+	else
+		if [ -f "$1" ]
+		then
+			decrypt_one $1
+			rm -f $1
+		else
+			echo -e
+			echo -e "File or directory does not exist."
+			echo -e
+		fi
+	fi
+fi
---
+++ enc.sh
@@ -0,0 +1,97 @@
+function check_end_char()
+{
+	len=${#1}
+	end_char=${1:len-1}
+	if [ $end_char == "/" ]
+	then
+		revised_name=${1:0:len-1}
+		echo $revised_name
+	else
+		echo $1
+	fi
+}
+
+function encrypt_one()
+{
+	if [ -d "$1" ]
+	then
+		fn=$(check_end_char $1)
+		# Encrypt a directory
+		echo -ne "$fn... "
+		echo -ne "\031[0;33mCompressing\033[0m] ..."
+		tar zcvf "$fn.tar.gz" "$fn"
+		echo -ne "\032[0;33mEncrypting\033[0m] ..."
+		openssl enc -des3 -e -in "$fn.tar.gz" -out "$fn.tar.gz.des3" -kfile ~/Desktop/.key
+		echo -e "\033[0;32mdone\033[0m"
+
+		if [ -f "$fn.tar.gz.des3" ]
+		then
+			rm -f "$fn.tar.gz"
+			rm -rf "$fn"
+		fi
+	else
+		if [ -f "$1" ]
+		then
+			# Encrypt a file
+			len=${#1}
+			check_gz=${1:len-3:$len}
+			check_des3=${1:len-5:$len}
+			in="$1"
+			out="$1.des3"
+
+			# The file has been encrypted.
+			if [ $check_des3 == ".des3" ]
+			then
+				echo -e "\033[0;31mNo encryption necessary.\033[0m"
+				exit 1;
+			fi
+
+			# Check if the file has been compressed
+			echo -ne "$1 ... "
+			if [ $check_gz != ".gz" ]
+			then
+				echo -ne "\033[0;34mCompressing\033[0m ... "
+				gzip -9 "$1"
+				in="$1.gz"
+				out="$1.gz.des3"
+			fi
+
+			echo -ne "\033[0;35mEncrypting\033[0m ... "
+			openssl enc -des3 -e -in "$in" -out "$out" -kfile ~/Desktop/.key
+			echo -e "\033[0;32mdone\033[0m"
+
+			if [ -f "$out" ]
+			then
+				rm -f "$in"
+			fi
+
+				else
+					echo -e
+					echo -e "File or directory does not exist."
+					echo -e
+				fi
+			fi
+}
+
+function encrypt_all()
+{
+	for i in *
+	do
+		encrypt_one "$i"
+	done
+}
+
+# Check if argument is given.
+if [ -z "$1" ]
+then
+	# If there is no given argument, recursively encrypt file one-by-one.
+	echo -e
+	echo -e "\033[1;32m === Encrypting all files under current directory \033[0m"
+	echo -e
+	encrypt_all
+	echo -e
+	echo -e
+	echo -e "\033[1;32m === Encryption finished === \033[0m"
+else
+	encrypt_one "$1"
+fi
